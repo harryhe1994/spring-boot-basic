40f4d78f8610de9c5977222d7f3c392631c855d7
Harry He <harry.r.he@oocl.com>
2018-05-22 15:50:45 +0800

    test

--- src/main/java/com/example/demo/domain/Fibonacci.java
+++ src/main/java/com/example/demo/domain/Fibonacci.java
@@ -2,6 +2,7 @@ package com.example.demo.domain;
 
 public class Fibonacci {
 
+    private Fibonacci(){}
     public static int of(int num) {
         if(num > 2) {
             return of(num - 2) + of(num - 1);
